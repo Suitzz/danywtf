6fa2c8c1ce23615b9df3f4770ba8d08207780d0f
Rafael <147251953+Suitzz@users.noreply.github.com>
2023-11-19 22:31:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 let shareMenuVisible = false;
+let menuIcon;
 
 window.onload = function () {
-    document.querySelector(".menu-icon").addEventListener('click', toggleMenu);
+    menuIcon = document.querySelector(".menu-icon");
+}
+
+// Function to toggle the visibility of the TikTok dropdown menu and move the WhatsApp button
+function toggleTikTokDropdown() {
+    const dropdown = document.getElementById('tiktokDropdown');
+    const whatsappButton = document.querySelector('.whatsapp-button');
+    const tiktokButton = document.getElementById('tiktokButton');
+
+    // Close the dropdown if it is already open and the TikTok button is selected
+    if (dropdown.style.display === 'block' && tiktokButton.classList.contains('selected')) {
+        dropdown.style.display = 'none';
+        tiktokButton.classList.remove('selected');
+        whatsappButton.style.marginTop = '10px'; // Reset the margin
+    } else {
+        // Toggle the visibility of the TikTok dropdown menu
+        dropdown.style.display = 'block';
+        tiktokButton.classList.add('selected');
+        
+        // Move the WhatsApp button down if the dropdown is visible
+        whatsappButton.style.marginTop = '100px'; // Adjust the margin as needed
+    }
+}
+
+// Function to navigate to the TikTok profile
+function navigateToProfile(lang) {
+    const tiktokProfileUrls = {
+        'portuguese': 'https://www.tiktok.com/@sapienz_pt',
+        'english': 'https://www.tiktok.com/@sapienz_en?lang=pt-BR',
+    };
+
+    window.location.href = tiktokProfileUrls[lang] || tiktokProfileUrls['english'];
+}
+
+// Function to navigate to TikTok videos
+function navigateToVideos(lang) {
+    const tiktokVideosUrls = {
+        'portuguese': 'https://www.tiktok.com/@sapienz_pt',
+        'english': 'https://www.tiktok.com/@sapienz_en?lang=pt-BR',
+    };
+
+    window.location.href = tiktokVideosUrls[lang] || tiktokVideosUrls['english'];
 }
 
 function toggleMenu(event) {
     const shareMenu = document.querySelector(".share-menu");
     const overlay = document.getElementById("overlay");
 
-    if (!shareMenu.contains(event.target) && shareMenuVisible) {
+    if (menuIcon && !shareMenu.contains(event.target) && !menuIcon.contains(event.target) && shareMenuVisible) {
         shareMenu.style.display = "none";
         overlay.style.display = "none";
         shareMenuVisible = false;
@@ -19,50 +61,86 @@ function toggleMenu(event) {
     }
 }
 
-document.body.addEventListener('click', function () {
+function closeMenu() {
     const shareMenu = document.querySelector(".share-menu");
     const overlay = document.getElementById("overlay");
 
     shareMenu.style.display = "none";
     overlay.style.display = "none";
     shareMenuVisible = false;
+}
 
-    // Close the TikTok dropdown when clicking outside of it
-    const dropdown = document.getElementById('tiktokDropdown');
-    dropdown.style.display = 'none';
+function closePopup() {
+    const popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
 
-    // Reset the margin of the WhatsApp button when the dropdown is closed
-    const whatsappButton = document.querySelector('.whatsapp-button');
-    whatsappButton.style.marginTop = '10px';
+function shareOnSocialMedia(platform) {
+    const url = 'https://summerloyal.com'; // Replace with your website URL
+    let shareUrl;
+
+    switch (platform) {
+        case 'twitter':
+            shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`;
+            break;
+        case 'whatsapp':
+            shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`;
+            break;
+        case 'facebook':
+            shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+            break;
+        default:
+            shareUrl = url;
+    }
+
+    window.open(shareUrl, '_blank');
+}
+
+function copyLink() {
+    const url = 'https://summerloyal.com'; // Replace with your website URL
+    navigator.clipboard.writeText(url)
+        .then(() => {
+            alert('Link copied to clipboard!');
+        })
+        .catch((err) => {
+            console.error('Unable to copy link', err);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // ...
+
+    // Show Cookie Policies pop-up
+    document.getElementById('cookie-preference-link').addEventListener('click', function () {
+        document.querySelector('.popup-cookie').style.display = 'block';
+        document.querySelector('.overlay').style.display = 'block';
+    });
+
+    // Close Cookie Policies pop-up when clicking outside the box
+    document.querySelector('.overlay').addEventListener('click', closeCookiePopup);
 });
 
-// Function to toggle the visibility of the TikTok dropdown menu
-function toggleTikTokDropdown() {
-    const dropdown = document.getElementById('tiktokDropdown');
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-
-    // Move the WhatsApp button down 20px when the TikTok dropdown is displayed
-    const whatsappButton = document.querySelector('.whatsapp-button');
-    if (dropdown.style.display === 'block') {
-        whatsappButton.style.marginTop = '30px'; // Adjust the margin as needed
-    } else {
-        whatsappButton.style.marginTop = '10px'; // Reset the margin when the dropdown is hidden
-    }
+function closeCookiePopup() {
+    document.querySelector('.popup-cookie').style.display = 'none';
+    document.querySelector('.overlay').style.display = 'none';
 }
 
 // Add click event listener to the TikTok button
 document.getElementById('tiktokButton').addEventListener('click', function (event) {
-    // Toggle the TikTok dropdown
+    // Prevent the click event from propagating to the body and closing the dropdown immediately
+    event.stopPropagation();
     toggleTikTokDropdown();
-
-    // Prevent the click event from reaching the body and closing the dropdown immediately
-    event.stopPropagation();
 });
 
-// Add click event listener to the TikTok dropdown items
-document.getElementById('tiktokDropdown').addEventListener('click', function (event) {
-    // Stop the click event from reaching the body and closing the dropdown immediately
-    event.stopPropagation();
-});
+// Close the dropdown when clicking outside of it
+document.body.addEventListener('click', function () {
+    const dropdown = document.getElementById('tiktokDropdown');
+    const tiktokButton = document.getElementById('tiktokButton');
+    const whatsappButton = document.querySelector('.whatsapp-button');
 
-// Additional functions (if any) can be added below this line.
+    if (dropdown.style.display === 'block' && tiktokButton.classList.contains('selected')) {
+        dropdown.style.display = 'none';
+        tiktokButton.classList.remove('selected');
+        whatsappButton.style.marginTop = '10px'; // Reset the margin
+    }
+});
